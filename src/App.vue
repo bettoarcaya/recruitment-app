61768ef42eb2caf44c5265b30c95fc82b5bdc188
Humberto Arcaya <betoarcaya@gmail.com>
2020-05-27 16:47:15 -0400

    adding the html structure to list all the availble jobs

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,11 @@
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <router-link 
+            to="/jobs" 
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >Jobs</router-link>
+          <!--<a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             Docs
           </a>
           <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
@@ -25,11 +29,11 @@
           </a>
           <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
             Blog
-          </a>
+          </a>-->
         </div>
-        <div>
+        <!--<div>
           <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-        </div>
+        </div>-->
       </div>
     </nav>
     <router-view/>
