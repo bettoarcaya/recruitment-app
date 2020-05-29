9a031bedda15375cbc68dc533ab2b8498b139f82
Humberto Arcaya <betoarcaya@gmail.com>
2020-05-28 21:23:00 -0400

    creating a job component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Job from '../views/Job.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/jobs',
-    name: 'Home',
-    component: Home,
+    name: 'Job',
+    component: Job,
   },
   {
     path: '/about',
---
+++ src/views/Job.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+    <div></div>
+    <div class="container mx-auto border-b border-black-400">
+      <h1 class="float-left">Job list</h1>
+      <button class="btn btn-blue float-right">
+        Add new job
+      </button>
+    </div>
+    <div class="container mx-auto">
+      <!--<img alt="Vue logo" src="../assets/logo.png" class="object-none object-center">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+      <div class="max-w-sm w-full lg:max-w-full lg:flex">
+        <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+          <img 
+            class="w-100 h-100 mr-4" 
+            alt="Vue logo" 
+            src="../assets/logo.png"
+          >
+        </div>
+        <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div class="mb-8">
+            <!--<p class="text-sm text-gray-600 flex items-center">
+              <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+              </svg>
+              Members only
+            </p>-->
+            <div class="text-gray-900 font-bold text-xl mb-2 text-left">Job title</div>
+            <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+          </div>
+          <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full mr-4" src="../assets/logo.png" alt="Avatar of Jonathan Reinink">
+            <div class="text-sm">
+              <p class="text-gray-900 leading-none">Commpany name</p>
+              <p class="text-gray-600">Aug 18</p>
+            </div>
+            <div class="container">
+              <button class="btn btn-green float-right">
+                Find candidates
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Job',
+}
+</script>
