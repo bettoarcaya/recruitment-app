53c6b0a012276c88af3cb32d1859e6a285bb2bef
Humberto Arcaya <betoarcaya@gmail.com>
2020-06-01 14:48:45 -0400

    adding header content to new components

--- src/views/Candidate.vue
+++ src/views/Candidate.vue
@@ -1,9 +1,22 @@
 <template>
-  
+	<div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+		<div></div>
+		<div class="container mx-auto border-b border-black-400">
+			<h1 class="float-left font-bold text-2xl">{{ name }}</h1>
+			<button class="btn btn-blue float-right">
+				Add new candidate
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-
+	name: 'Candidate',
+	data(){
+		return {
+			name: 'Candidate list'
+		}
+	}
 }
 </script>
--- src/views/Match.vue
+++ src/views/Match.vue
@@ -1,9 +1,19 @@
 <template>
-  
+	<div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+		<div></div>
+		<div class="container mx-auto border-b border-black-400">
+			<h1 class="float-left font-bold text-2xl">{{ name }}</h1>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-
+	name: 'Match',
+	data(){
+		return {
+			name: 'Matches'
+		}
+	}
 }
 </script>
