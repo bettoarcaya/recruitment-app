d34dbe749d15083dcf064c76b1db23ea920594f2
Humberto Arcaya <betoarcaya@gmail.com>
2020-06-02 12:39:36 -0400

    adding the neccesay information

--- src/views/Candidate.vue
+++ src/views/Candidate.vue
@@ -23,15 +23,27 @@
 					</div>
 					<div class="px-6 py-4">
 						<div class="font-bold text-xl mb-2"> {{candidate.firstname}} {{candidate.lastname}} </div>
-						<p class="text-gray-700 text-base">
+						<!--<p class="text-gray-700 text-base">
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-						</p>
+						</p>-->
+						<div class="flex mb-4">
+							<div class="w-1/2 text-left">
+								<p>Age: </p>
+								<p>Email: </p>
+								<p>Profile: </p>
+							</div>
+							<div class="w-1/2 text-left">
+								<p>24</p>
+								<p>{{candidate.email}}</p>
+								<p>Dev</p>
+							</div>
+						</div>
 					</div>
-					<div class="px-6 py-4">
+					<!--<div class="px-6 py-4">
 						<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
 						<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
 						<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-					</div>
+					</div>-->
 				</div>
 			</div>
 		</div>
