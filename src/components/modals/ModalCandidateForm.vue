2bf71a84c90301e0107443a9436b91183d6f2a13
Humberto Arcaya <betoarcaya@gmail.com>
2020-06-08 18:27:53 -0400

    adding header title to the first step modal

--- src/components/modals/ModalCandidateForm.vue
+++ src/components/modals/ModalCandidateForm.vue
@@ -3,8 +3,13 @@
 		<div class="modal-mask">
 			<div class="modal-content">
 				<div class="modal-header">
+					<a href="javascript:void(0)" @click="$emit('close')">
+						<span class="close float-right">&times;</span>
+					</a>
+					<h2 class="text-left font-bold text-xl">Add new candidate</h2>
 				</div>
 				<div class="modal-body">
+					<h2 class="text-center font-bold text-sm">Personal data</h2>
 				</div>
 				<div class="modal-footer">
 				</div>
