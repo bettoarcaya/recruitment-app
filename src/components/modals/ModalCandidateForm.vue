633617885344cd19fb8070c31df9b4b9839b03ce
Humberto Arcaya <betoarcaya@gmail.com>
2020-06-08 18:45:08 -0400

    creating the neccesary components to steps form

--- src/components/modals/ModalCandidateForm.vue
+++ src/components/modals/ModalCandidateForm.vue
@@ -9,9 +9,22 @@
 					<h2 class="text-left font-bold text-xl">Add new candidate</h2>
 				</div>
 				<div class="modal-body">
-					<h2 class="text-center font-bold text-sm">Personal data</h2>
+					<personal-data-form v-if="step == 1"></personal-data-form>
+					<background-data-form v-if="step == 2"></background-data-form>
+					<work-experience-data-form v-if="step == 3"></work-experience-data-form>
 				</div>
 				<div class="modal-footer">
+					<div class="inline-flex">
+						<button class="btn btn-blue" @click="$emit('close')">
+							Cancel
+						</button>
+						<button 
+							class="btn btn-blue margin-l-5" 
+							@click="next"
+						>
+							Next
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -19,7 +32,22 @@
 </template>
 
 <script>
+import PersonalDataForm from '@/components/candidate/PersonalDataForm.vue';
+import BackgroundDataForm from '@/components/candidate/BackgroundDataForm.vue';
+import WorkExperienceDataForm from '@/components/candidate/WorkExperienceDataForm.vue';
+
 export default {
+	components: {
+		PersonalDataForm,
+		BackgroundDataForm,
+		WorkExperienceDataForm
+	},
+	data() {
+		return {
+			step: 1,
+			steps: ['Personal data', 'Backgrounds', 'Work experiences']
+		}
+	}
 
 }
 </script>
